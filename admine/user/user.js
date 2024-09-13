5b92d3372e202c35278e077c9aeb81310fef90a9
azizromdhani7 <rromdhanimmohamedaziz@gmail.com>
2024-09-13 11:29:06 +0100

    problem in the search

--- admine/user/user.js
+++ admine/user/user.js
@@ -18,21 +18,69 @@ function each(array, func) {
       }
     });
     return acc}
+    //
     function factory(title,duration,descreption,category,rating,pictures){
-        var obj={
-        title:title,
+return{title:title,
         duration:duration,
         descreption:descreption,
       category:category,
       rating:rating,
-      pictures:pictures,
+      pictures:pictures}
+    }
+    
+  //
+    function film(){
+
+      const add=function(title,duration,descreption,category,rating,pictures){
+      return this.list.push(factory(title,duration,descreption,category,rating,pictures))}
+
+ const search=function(titre){
+        return filter(this.list,function(elem,i){
+            return this.list.title===titre
+        })
+      }
+        var obj={
+        list:[],
+      add:add,
       search:search, 
         }
       return obj
       }
-      const search=function(titre){
-        return filter(list,function(elem,i){
-            return list.title===titre
-        })
+      function displayOne(film){
+        $(".displays").append(`<div class = "mov"> 
+          <img src="${film.pictures}">
+             <h3 id =""> Name :${film.title}</h3>
+             <h3 id =""> Name :${film.category}</h3>
+             <h3 id="">durartion:${film.duration}</h3>
+           <h3 id ="">rating:${film.rating }</h3>
+           <h3 id ="">descreption: ${film.descreption}</h3>
+           <a id="mm" href=""><h3>get it</h3></a>  
+             </div>`)  
       }
-      
+
+      function displayAll(array){
+        console.log(array)
+          for (let i = 0; i < array.length; i++) {
+              const element = array[i];
+              displayOne(element)
+          }
+      }
+     
+
+var movies=film()
+movies.add("inception",148,"great movie ever","fun",7.6,"https://www.shutterstock.com/image-vector/detailed-illustration-clapper-board-short-260nw-210237415.jpg")
+movies.add("ince",148,"great movie ever","fun",7.6,"https://www.shutterstock.com/image-vector/detailed-illustration-clapper-board-short-260nw-210237415.jpg")
+displayAll(movies.list)
+ function research() {
+        var sear = $("#B-recherche").val();
+        $(".displays").empty();
+     var x=  movies.list.search(sear);
+     console.log(x.length)
+     if(x.length===0){
+        alert('          ***not found***')
+     }
+     displayAll(x)
+      }
+      $("#SBT").on("click", function () {
+        this.search();
+      });
